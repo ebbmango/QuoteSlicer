30045d2b127a2cecc3fb64af365cff64041c1cc5
Emanuel Borges <ebbmango@tuta.io>
2025-10-20 10:30:48 +0200

    generate buttons from data (no hardcoding)

--- src/lib/ModeSelector.svelte
+++ src/lib/ModeSelector.svelte
@@ -8,8 +8,12 @@
   import ModeSelectorButton from "./ModeSelectorButton.svelte";
 
   const mode: Mode = getContext("mode");
-  const verse: boolean = $derived(mode.current === 2 || mode.current === 3);
-  const write: boolean = $derived(mode.current === 0);
+
+  const modes = new Map([
+    [1, { label: "Map", icon: icons.highlighter.path }],
+    [2, { label: "Versify", icon: icons.paragraph.path }],
+    [3, { label: "Preview", icon: icons.eye.path }],
+  ]);
 
   // PLAN: start in mode 0; when the mode switches, the keyboard
   // icon should fly out as the mode selector flies in.
@@ -20,8 +24,8 @@
   // FIX: delay rendering the icon/menu of the new interaction
   // mode until the old one's outro animation finishes.
 
-  let zero = $state(mode.current === 0); // true = text (0) / false = modes (1, 2, 3)
-  let show = $state(true); // controls render, allowing for its delay
+  let zero: boolean = $state(mode.current === 0); // true = text (0) / false = modes (1, 2, 3)
+  let show: boolean = $state(true); // controls render, allowing for its delay
 
   $effect(() => {
     const next = mode.current === 0;
@@ -55,63 +59,15 @@
     onoutroend={delay}
     class="flex gap-3 fill-tin h-[3dvh] duration-200 z-10"
   >
-    <!-- MAPPING -->
-    <ModeSelectorButton
-      label="Map"
-      icon={icons.highlighter.path}
-      dimmed={mode.current !== 1}
-      click={() => {
-        mode.current = 1;
-      }}
-    />
-    <!-- VERSIFICATION -->
-    <ModeSelectorButton
-      label="Versify"
-      icon={icons.paragraph.path}
-      dimmed={!verse}
-      click={() => {
-        if (!verse) mode.current = 2;
-      }}
-    />
-    <!-- PREVIEW -->
-    <ModeSelectorButton
-      label="Preview"
-      icon={icons.eye.path}
-      dimmed={mode.current !== 4}
-      click={() => {
-        mode.current = 4;
-      }}
-    />
+    {#each modes as [number, properties]}
+      <ModeSelectorButton
+        label={properties.label}
+        icon={properties.icon}
+        dimmed={mode.current !== number}
+        click={() => {
+          mode.current = number;
+        }}
+      />
+    {/each}
   </div>
-  <!-- bottom row -->
-  <!-- this should only be shown if the versification mode is active -->
-  {#if verse}
-    <div
-      in:fly={{ y: -10, duration: 500 }}
-      out:fly={{ x: write ? 200 : 0, duration: write ? 500 : 300 }}
-      class="flex gap-3 fill-tin h-[3dvh]"
-      class:-translate-y-4={!verse}
-    >
-      <!-- JOIN -->
-      <ModeSelectorButton
-        label="Join verses"
-        icon={icons.join.path}
-        rotate
-        dimmed={mode.current !== 2}
-        click={() => {
-          mode.current = 2;
-        }}
-      />
-      <!-- SPLIT -->
-      <ModeSelectorButton
-        label="Split verses"
-        icon={icons.separate.path}
-        rotate
-        dimmed={mode.current !== 3}
-        click={() => {
-          mode.current = 3;
-        }}
-      />
-    </div>
-  {/if}
 {/if}
